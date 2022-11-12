82c75c66ddfa5b8ec4a8134c019cdf94d76f7b6c
Texcean <texcean@protonmail.com>
2022-11-12 19:11:17 +0800

    Create LA05.java

---
+++ LA05.java
@@ -0,0 +1,16 @@
+public class LA05 {
+
+	public static void main(String[] args) {
+		
+		char sampChat = 'a';
+		int age = 18;
+		float PI = 3.14f;
+		boolean ans = true;
+	
+		String fullName = "James Michael R. Santos";
+		
+		System.out.println("Hello World!" + fullName);
+
+	}
+
+}
