e8b1e94c337dde96c648cfe26fa83b8cc8c13a85
Texcean <texcean@protonmail.com>
2022-11-12 19:12:25 +0800

    Create LA04.java

---
+++ LA04.java
@@ -0,0 +1,14 @@
+public class LA04 {
+
+	public static void main(String[] args) {
+		String myName;
+		myName = "James Michael R.Santos";
+		
+		System.out.println("Hello! I am " + myName);
+		System.out.println("Welcome To The World Of ICT's!");
+		System.out.println("Ang pogi ko talaga");
+		
+	}
+
+}
+ 
