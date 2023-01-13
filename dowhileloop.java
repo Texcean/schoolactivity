094c3e9f928b968e74e8d4eccbf5d2900518dc4b
Texcean <113755199+Texcean@users.noreply.github.com>
2023-01-13 17:26:26 +0800

    Create dowhileloop.java

---
+++ dowhileloop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class dowhileloop {
+    public static void main(String[] args) {
+        Scanner jm = new Scanner(System.in);
+        
+        String tName = "";
+        
+        while (!tName.equals("STOP")) {
+            System.out.println("Enter any Name: " + tName);
+            tName = jm.next();
+            
+            // James Michael R. Santos
+            // ICT - MESSENGER
+        }
+    }
+}
