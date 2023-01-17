14b4b43296b69be3d66d725480fc99fda910f9f8
Texcean <113755199+Texcean@users.noreply.github.com>
2023-01-17 17:42:36 +0800

    Update dowhileloop.java

--- dowhileloop.java
+++ dowhileloop.java
@@ -5,10 +5,13 @@ class dowhileloop {
         Scanner jm = new Scanner(System.in);
         
         String tName = "";
-        
+        int n = 0;
+
         while (!tName.equals("STOP")) {
-            System.out.println("Enter any Name: " + tName);
+            System.out.println("Enter any Name: " + n + ": " + tName);
             tName = jm.next();
+            n++;
+
             
             // James Michael R. Santos
             // ICT - MESSENGER
